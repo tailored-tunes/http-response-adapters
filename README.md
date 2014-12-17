150a925abe54a8fc525d2d029d5c17fa77d359d0
meza <meza@meza.hu>
2014-12-17 10:15:36 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+http-response-adapters
+========================
+
+A collection of http responses
+
+# Installation
+
+Install via composer. Installation help and versions at [Packagist](https://packagist.org/packages/tailored-tunes/http-response-adapters)
+
---
+++ src/TailoredTunes/HttpResponses/JsonResponse.php
@@ -0,0 +1,41 @@
+<?php
+namespace TailoredTunes\HttpResponses;
+
+class JsonResponse
+{
+
+
+    private $success;
+    private $data;
+
+    public function __construct($success, $data)
+    {
+        $this->success = $success;
+        $this->data = $data;
+    }
+
+    public function response($code = 200, $headers = array())
+    {
+        $response = new Response();
+
+        $body = json_encode(
+            [
+                'success' => $this->success,
+                'data' => $this->data
+            ]
+        );
+
+        $defaultOptions = [
+            'Content-Type' => 'application/json',
+            'Content-Length' => strlen($body)
+        ];
+
+        $finalHeaders = array_merge($defaultOptions, $headers);
+
+        $response->setHeaders($finalHeaders);
+        $response->setBody($body);
+        $response->respondWith($code);
+
+        return $response;
+    }
+}
